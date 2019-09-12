227ac7fe53476f525827af493379521a3215eb5e
Atsushi Hosaka <ahosaka@indeed.com>
2019-09-11 21:31:21 -0500

    Updates

---
+++ precache-manifest.4d3a35817573655aa92593cabf4ade3e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c35244040b1a1660968e87786f6cbfb",
+    "url": "/can_you_even_lift/index.html"
+  },
+  {
+    "revision": "a1ac62f7702b8edc9d45",
+    "url": "/can_you_even_lift/static/css/2.58e42683.chunk.css"
+  },
+  {
+    "revision": "d357ade3123e22faa8e8",
+    "url": "/can_you_even_lift/static/css/main.d4189a9b.chunk.css"
+  },
+  {
+    "revision": "a1ac62f7702b8edc9d45",
+    "url": "/can_you_even_lift/static/js/2.8b22e36b.chunk.js"
+  },
+  {
+    "revision": "d357ade3123e22faa8e8",
+    "url": "/can_you_even_lift/static/js/main.0c12397a.chunk.js"
+  },
+  {
+    "revision": "b6a56afee6eeb93633f0",
+    "url": "/can_you_even_lift/static/js/runtime~main.c92cc8cb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/can_you_even_lift/static/media/logo.5d5d9eef.svg"
+  }
+]);
